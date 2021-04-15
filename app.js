1ebb61fa6ca7e87e4040f0e0322fc5afb081fe9a
Mahmoud Abdelrahman <47402937+JS-player@users.noreply.github.com>
2021-04-15 15:26:22 +0200

    Fixing Select Value

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var dayName = days[today.getDay()];
 var day = today.getDate();
 var m = today.getMonth() + 1;
 const month = monthNames[m];
-var date1 = dayName + ', ' + day + ' ' + month + '' + today.getFullYear();
+var date1 = dayName + ', ' + day + ' ' + month + ' ' + today.getFullYear();
 var temp = 0;
 //================================================================================
 const apiKey = '9ad883bdccd12827fc9c35fd6402a466';
@@ -18,7 +18,8 @@ async function getDate() {
     var fullUrll = `https://api.openweathermap.org/data/2.5/weather?zip=${Zipcode}&appid=${apiKey}&units=metric`;
     var city = `Zipcode: ${Zipcode}`;
     if (!Zipcode) {
-      var city = document.getElementById('citySel').value;
+      var Cnum = document.getElementsByTagName('select')[0].selectedIndex;      
+      var city = document.getElementsByTagName('select')[0][Cnum].innerText
       var fullUrll = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     }
     document.getElementById('loc').innerHTML = `${city}`;
